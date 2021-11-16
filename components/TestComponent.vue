8f96b5e204f424191330ac288a6bd5639f6b2ff1
Jessica.W <jessica.watson1@live.com>
2021-11-16 13:04:41 -0700

    initial com

---
+++ components/TestComponent.vue
@@ -0,0 +1,7 @@
+<template>
+  <section>
+    <h2>Having one component is nice</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit quae in illo aperiam odio, voluptatem distinctio a inventore dolorem atque deserunt ratione necessitatibus sequi aliquid fugiat laudantium, itaque aut quo dignissimos nostrum id corporis. Soluta veritatis harum autem rem blanditiis aspernatur nisi ad nostrum magni!</p>
+    <nuxt-img src="assets/Yuzis.jpg" />
+  </section>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <TestComponent />
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
